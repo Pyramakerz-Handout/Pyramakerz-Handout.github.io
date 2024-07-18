87b9908b67c172514f95a9e99a601efc238c982a
Pyramakerz-Handout <abdelmenamtarek.forwork@gmail.com>
2024-07-18 14:49:06 +0300

    Update2

--- additem.js
+++ additem.js
@@ -6,7 +6,7 @@ const galleryItems = [
     {imgSrc: "gallery-4",title: "APP Inventor",link: "b_App_inventor"},
     {imgSrc: "gallery-5",title: "Programming universe Seniors",link: "b_programming_universe"},
     {imgSrc: "gallery-6",title: "Programming universe Junior",link: "b_programming_universe_junior"},
-    {imgSrc: "gallery-7",title: "Programming universe Junior",link: "b_web"},
+    {imgSrc: "gallery-7",title: "Web Development",link: "b_web"},
   ];
   
   // Function to create gallery item HTML
